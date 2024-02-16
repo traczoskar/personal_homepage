598dc5292fab69ff48c1ee77d633d60f8ac9ddb4
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-02-16 18:52:33 +0100

    added theme provider and redux store provider for overall App

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { GlobalStyle } from "./GlobalStyle.js";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./theme.js";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
 
