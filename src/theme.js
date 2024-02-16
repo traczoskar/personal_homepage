054b31537e18136904244b3e3e2f2a485c625a69
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-02-16 18:53:09 +0100

    added initial colors theme

---
+++ src/theme.js
@@ -0,0 +1,19 @@
+export const theme = {
+  color: {
+    white: "#FFFFFF",
+    whitelilac: "#FBFBFE",
+    alabaster: "#F8F8F8",
+    gallery: "#EEEEEE",
+    alto: "#DDDDDD",
+    silver: "#CCCCCC",
+    darksilver: "#BBBBBB",
+    mineshaft: "#333333",
+    black: "#000000",
+    teal: "#008080",
+    bilbao: "#327C14",
+    milanored: "#B81104",
+  },
+  breakpoint: {
+    mobileMax: 767,
+  },
+};
