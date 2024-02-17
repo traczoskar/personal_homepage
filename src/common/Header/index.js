a5a12afeadcee667bb68d6c4b87ffa3e0fff6ef8
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-02-17 13:54:24 +0100

    added styled component Header

---
+++ src/common/Header/index.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Header = styled.h2`
+  display: flex;
+  align-items: center;
+  font-size: 30px;
+  font-weight: 900;
+  max-width: 1151px;
+  margin: 0;
+  padding: 13px 0;
+  border-bottom: 1px solid rgba(209, 213, 218, 0.3);
+`;
