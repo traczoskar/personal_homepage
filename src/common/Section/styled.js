3c4eb2d4d776a5700cfbcd7cb442d8ad447c0b92
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-02-17 23:28:55 +0100

    section componen styling modified

--- src/common/Section/styled.js
+++ src/common/Section/styled.js
@@ -4,5 +4,6 @@ export const Section = styled.section`
   padding: 32px 33px 32px 32px;
   background-color: ${({ theme }) => theme.color.white};
   box-shadow: 0px 16px 58px 0px rgba(9, 10, 51, 0.03);
-  /* box-shadow: 0px -2px 50px 0px rgba(9, 10, 51, 0.02); */
+  box-shadow: 0px -2px 50px 0px rgba(9, 10, 51, 0.02);
+  margin-bottom: 72px;
 `;
