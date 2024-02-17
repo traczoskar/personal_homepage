95471a1669de2fd505c89492c79ecdeb05c07dd2
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-02-17 13:31:01 +0100

    added dataStore file

---
+++ src/dataStore.js
@@ -0,0 +1,50 @@
+export const personalData = {
+  name: "Oskar",
+  surname: "Tracz",
+  aboutMe:
+    "👨🏻‍💻🖥 Passionate Frontend Developer with a keen eye for user-centric design and visual details. I am seeking engaging projects while dedicating my free time to building my own SaaS application, using experience gained in other industries.",
+  email: "traczoskar@gmail.com",
+};
+
+export const skillList = [
+  "Semantic HTML",
+  "CSS: Flex box",
+  "CSS: Grid",
+  "JavaScript ES6+",
+  "CSS: media queries",
+  "CSS: animations/keyframes",
+  "Styled Components",
+  "Figma",
+  "Trello",
+  "Teamwork",
+  "React.js",
+  "React Hooks",
+  "React Router",
+  "Immutability",
+  "npm",
+  "GitHub: Pull requests and review",
+  "Git",
+  "Error handling",
+  "Working with REST API (fetch, axios)",
+  "Promise, Async, Await",
+  "Redux",
+  "Redux-Saga",
+  "Redux (Toolkit)",
+  "React-Router",
+  "Scrum",
+  "Markdown",
+];
+
+export const futureSkillList = [
+  "SSR: Next.js",
+  "TypeScript",
+  "React Context",
+  "Node.js",
+  "Tailwind CSS",
+  "Unit testing",
+  "Vite",
+  "GraphQL",
+  "React Native",
+];
+
+export const GitHubAPI = "https://api.github.com/users/traczoskar/repos";
