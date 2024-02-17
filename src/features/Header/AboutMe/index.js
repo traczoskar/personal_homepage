43da880119058760a0fa23436c012f3115dbd397
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-02-17 23:30:53 +0100

    added styled-component AboutMe in Header

---
+++ src/features/Header/AboutMe/index.js
@@ -0,0 +1,16 @@
+import { personalData } from "../../../dataStore";
+import { Button } from "../Button";
+import { Description, Name, Title } from "./styled";
+
+export const AboutMe = () => {
+  return (
+    <div>
+      <Title>THIS IS</Title>
+      <Name>
+        {personalData.name} {personalData.surname}
+      </Name>
+      <Description>{personalData.caption}</Description>
+      <Button title="Hire Me" />
+    </div>
+  );
+};
