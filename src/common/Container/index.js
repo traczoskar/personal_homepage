6593b5e5b3425376f13ad68f48d999829ee5eea7
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-02-21 15:36:21 +0100

    changed main Container styling

--- src/common/Container/index.js
+++ src/common/Container/index.js
@@ -1,6 +1,12 @@
 import styled from "styled-components";
 
 export const Container = styled.main`
-  max-width: 1216px;
+  max-width: 1248px;
   margin: auto;
+  margin-top: 120px;
+  padding: 16px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+    padding: 8px;
+  }
 `;
