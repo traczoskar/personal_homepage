21fc42f40cc06c989642a7011c258e36698643a3
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-02-21 09:27:50 +0100

    changed folder and files organization

---
+++ src/features/PersonalHomepage/Header/styled.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: auto auto;
+  align-items: center;
+  gap: 72px;
+  margin-top: 119px;
+  margin-bottom: 73px;
+  padding: 0 128px 0 0;
+`;
+
+export const Photo = styled.img`
+  width: 398px;
+  height: 398px;
+  border-radius: 50%;
+`;
