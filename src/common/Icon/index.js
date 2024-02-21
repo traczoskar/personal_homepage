c38e7a2f7e29d468b637b096184b265fee3b2077
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-02-21 15:36:49 +0100

    changed styling to Icon and List common components

--- src/common/Icon/index.js
+++ src/common/Icon/index.js
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 
 export const Icon = styled.span`
-  font-size: 24px;
+  font-size: 30px;
   font-weight: 400;
   margin-left: 16px;
+  vertical-align: center;
 `;
--- src/common/List/styled.js
+++ src/common/List/styled.js
@@ -1,25 +1,55 @@
 import styled from "styled-components";
 import { ReactComponent as DiscIcon } from "../../assets/disc.svg";
+import { SubHeader } from "../SubHeader";
 
 export const StyledList = styled.ul`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
-  justify-content: start;
-  gap: 8px;
-  column-gap: 115px;
-  padding-left: 0;
+  grid-gap: 8px 32px;
+  padding: 0;
   margin-top: 32px;
   list-style: none;
-  line-height: 25px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tabletHorMax}px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tabletVerticalMax}px) {
+    font-size: 16px;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+    grid-template-columns: 1fr;
+    font-size: 14px;
+    margin-bottom: 12px;
+  }
 `;
 
 export const ListItem = styled.li`
+  display: flex;
+  line-height: 1.4;
   font-weight: 400;
-  color: ${({ theme }) => theme.color.slategray};
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+    line-height: 1.3;
+  }
 `;
 
 export const Disc = styled(DiscIcon)`
-  width: 9px;
-  height: 9px;
-  margin-right: 7px;
+  height: auto;
+  margin-right: 16px;
+  color: ${({ theme }) => theme.colors.basic};
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+    width: 7px;
+    margin-right: 8px;
+  }
+`;
+
+export const StyledSubHeader = styled(SubHeader)`
+  padding-bottom: 16px;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.headerLine};
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+    padding-bottom: 12px;
+  }
 `;
