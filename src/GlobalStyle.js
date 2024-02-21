7eb17b11678b6fb229f81d189d990b8606be76aa
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-02-21 09:26:31 +0100

    added styling to the body of the app

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -12,6 +12,11 @@ export const GlobalStyle = createGlobalStyle`
     body {
         font-family: 'Inter', sans-serif;
         word-break: break-word;
-        background-color: ${({ theme }) => theme.color.whitelilac};
+        transition: background 0.3s;
+        font-size: 18px;
+        background-color: ${({ theme }) => theme.colors.app.background};
+        color: ${({ theme }) => theme.colors.app.text};
+        letter-spacing: 0.05em;
+        padding-bottom: 108px;      
     }
 `;
