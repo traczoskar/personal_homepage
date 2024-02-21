935852734cd50ac0946cae8200849001061d2ed5
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-02-21 15:41:55 +0100

    changed List structure

--- src/common/List/index.js
+++ src/common/List/index.js
@@ -1,14 +1,21 @@
-import { Disc, StyledList, ListItem } from "./styled";
+import { Disc, StyledList, ListItem, StyledSubHeader } from "./styled";
+import { Icon } from "../Icon";
 
-export const List = ({ skills }) => {
+export const List = ({ skills, title, icon }) => {
   return (
-    <StyledList>
-      {skills.map((skill) => (
-        <ListItem key={skill}>
-          <Disc />
-          {skill}
-        </ListItem>
-      ))}
-    </StyledList>
+    <>
+      <StyledSubHeader>
+        {title}
+        <Icon>{icon}</Icon>
+      </StyledSubHeader>
+      <StyledList>
+        {skills.map((skill) => (
+          <ListItem key={skill}>
+            <Disc />
+            {skill}
+          </ListItem>
+        ))}
+      </StyledList>
+    </>
   );
 };
