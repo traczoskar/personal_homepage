16224e27b4420e97d35321916a6fb8d12d0bea66
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-02-21 19:14:31 +0100

    added few styling and wrapping components

--- src/features/PersonalHomepage/Footer/index.js
+++ src/features/PersonalHomepage/Footer/index.js
@@ -1,14 +1,25 @@
 import { personalData } from "../../../dataStore";
 import { SocialIcons } from "./SocialIcons";
-import { Caption, Description, Email, Wrapper } from "./styled";
+import {
+  Address,
+  Caption,
+  Description,
+  Email,
+  EmailWrapper,
+  Wrapper,
+} from "./styled";
 
 export const Footer = () => {
   return (
     <Wrapper>
-      <Caption>LET'S TALK!</Caption>
-      <Email href="mailto:traczoskar@gmail.com">{personalData.email}</Email>
-      <Description>{personalData.description}</Description>
-      <SocialIcons />
+      <Address>
+        <Caption>LET'S TALK!</Caption>
+        <EmailWrapper>
+          <Email href="mailto:traczoskar@gmail.com">{personalData.email}</Email>
+        </EmailWrapper>
+        <Description>{personalData.description}</Description>
+        <SocialIcons />
+      </Address>
     </Wrapper>
   );
 };
