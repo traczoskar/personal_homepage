a6199bb5b808550d7049d8196c91026a3de743a7
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-07-06 16:52:49 +0200

    feat: Enable dark theme by default in themeSlice.js

--- src/common/ThemeSwitch/themeSlice.js
+++ src/common/ThemeSwitch/themeSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const themeSlice = createSlice({
   name: "theme",
   initialState: {
-    isDarkTheme: false,
+    isDarkTheme: true,
   },
   reducers: {
     toggleTheme: (state) => {
