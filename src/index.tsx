6029e952e0fca854640d3b4ff8ca52cde718126d
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-07-15 23:25:09 +0200

    updated entry point file for TypeScript file format

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./core/App/App.js";
 import { Provider } from "react-redux";
 import store from "./core/store.js";
-import reportWebVitals from "./reportWebVitals";
+import reportWebVitals from "./reportWebVitals.js";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
