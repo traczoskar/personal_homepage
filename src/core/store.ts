7db6288393551a6cdf0abf8df293488ede3da68d
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-07-15 23:30:21 +0200

    changed Redux store file to TypeScript format

--- src/core/store.ts
+++ src/core/store.ts
@@ -1,17 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
-import createSagaMiddleware from "redux-saga";
+import createSagaMiddleware, { SagaMiddleware } from "redux-saga";
 import personalHomepageReducer from "../features/PersonalHomepage/personalHomepageSlice";
 import themeReducer from "../common/ThemeSwitch/themeSlice";
 import saga from "./saga";
 
-const sagaMiddleware = createSagaMiddleware();
+const sagaMiddleware: SagaMiddleware<object> = createSagaMiddleware();
 
 const store = configureStore({
   reducer: {
     personalHomepage: personalHomepageReducer,
     theme: themeReducer,
   },
-  middleware: () => [sagaMiddleware],
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(sagaMiddleware),
 });
 
 sagaMiddleware.run(saga);
