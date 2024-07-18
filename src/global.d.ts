a48187b3bc69d63816fc67baed85e952a123dc9d
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-07-18 15:33:19 +0200

    refactor: Update module declarations for image file types

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,14 +1,30 @@
+//---PNG declaration---
 declare module "*.png" {
   const value: any;
   export = value;
 }
-
+//---JPEG declaration---
 declare module "*.jpeg" {
   const value: any;
   export = value;
 }
-
+//---JPG declaration---
 declare module "*.jpg" {
   const value: any;
   export = value;
+}
+
+//---SVG declaration---
+declare module "*.svg" {
+  import * as React from "react";
+
+  interface SVGProps extends React.SVGProps<SVGSVGElement> {
+    stroke: string;
+  }
+
+  const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement> & { title?: string }
+  >;
+
+  export const ReactComponent;
 }
